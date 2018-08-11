a7d0767556e94ad892024335e1fce7a47caa7b23
Maximiliano de Mattos <maximiliano.x.de.mattos.ferraz.-nd@disney.com>
2018-08-11 13:07:25 -0300

    Add util and fix other files to wirk with it

---
+++ util/updateHubDocker.sh.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+# Autor:
+#   Maximiliano de Mattos (azamax@gmail.com)
+#
+# Description:
+#   Script to update images at hub.docker
+#
+# Usage:
+#   $ sh updateHubDocker.sh
+#
+################################################################################
+tabs 4
+set -e
+nocolor='\e[0m'; cyan='\e[0;36m'
+
+HUB_DOCKER_USER="maximatt"
+HUB_IMAGES=(`docker search $HUB_DOCKER_USER | grep $HUB_DOCKER_USER | cut -d' ' -f1`)
+HUB_IMAGES_REV=(`tac -s" " <<< "${HUB_IMAGES[@]} "`)
+
+declare -A DOCKER_FILES=(
+    [default]="../docker-compose.yml"
+    [tor-service]="../tor/docker-compose.yml"
+)
+
+deleteLocally(){
+    # $1: hub_image
+    image_id=$(docker images | grep $1 | awk "{print \$3}")
+    if [ ! -z "$image_id" ]; then
+        printf "$cyan image deleted locally $nocolor\n"
+        docker rmi -f $image_id
+    fi
+}
+
+pullImage(){
+    # $1: hub_image
+    # $2: tag
+    image_id=$(docker images | grep $1 | awk "{print \$3}")
+    if [ -z "$image_id" ]; then
+        printf "$cyan \tpull image $nocolor\n"
+        docker pull $1:$2
+    fi
+}
+
+buildImage(){
+    # $1: hub_image
+    image=${hub_image/maximatt\//}
+    if [ -z "${image##*tor-service*}" ]; then
+        docker_compose_file=${DOCKER_FILES[$image]}
+    else
+        docker_compose_file=${DOCKER_FILES['default']}
+    fi
+    
+    printf "$cyan building $image $nocolor\n"
+    docker-compose -f $docker_compose_file build $image
+}
+
+pushImage(){
+    # $1: image
+    # $2: tag
+    image=${1/maximatt\//}
+    printf "$cyan push $image:latest -> $HUB_DOCKER_USER/$image:$2 $nocolor\n"
+    docker tag $image:latest $HUB_DOCKER_USER/$image:$2
+    docker push $HUB_DOCKER_USER/$image:$2
+}
+
+########
+# MAIN #
+########
+
+for hub_image in "${HUB_IMAGES_REV[@]}"; do
+    deleteLocally $hub_image
+done
+
+for hub_image in "${HUB_IMAGES[@]}"; do
+    if [ -z "${hub_image##*centos7_i386*}" ]; then
+        continue # centos7_i386 is built from scratch
+    fi
+    buildImage $hub_image
+    pushImage $hub_image "1.0"
+    pushImage $hub_image "latest"
+    docker rmi "$hub_image:1.0"
+    docker rmi "${hub_image/maximatt\//}:latest"
+done
